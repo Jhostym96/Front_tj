9d496752705a85b64159fc0e8510c89051eb18f0
Jhostym <you@example.com>
2023-03-07 15:31:17 -0500

    update

--- src/Components/ControlTrabajo/ListControl/index.jsx
+++ src/Components/ControlTrabajo/ListControl/index.jsx
@@ -7,38 +7,60 @@ function ListControl() {
 
     const { user } = useContext(AuthContext);
 
-    const [datos, setDatos] = useState(null);
+    const [datos, setDatos] = useState([]);
+    const [fechaInicio, setFechaInicio] = useState('');
+    const [fechaFin, setFechaFin] = useState('');
 
-
-    const todosLosServicios = async () => {
+    const obtenerServicios = async () => {
         try {
             const response = await axios.get('http://localhost:5000/api/v1/maquinaria', {
                 headers: {
                     Authorization: `bearer ${user.token}`
+                },
+                params: {
+                    fechaInicio,
+                    fechaFin
                 }
-            })
+            });
 
-            setDatos(response?.data.servicios);
-
+            setDatos(response?.data.servicios || []);
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
-    }
+    };
 
     useEffect(() => {
-        todosLosServicios();
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        const fechaActual = new Date();
+        const primerDiaDelMes = new Date(fechaActual.getFullYear(), fechaActual.getMonth(), 1);
+        const ultimoDiaDelMes = new Date(fechaActual.getFullYear(), fechaActual.getMonth() + 1, 0);
+        setFechaInicio(primerDiaDelMes.toISOString().slice(0, 10));
+        setFechaFin(ultimoDiaDelMes.toISOString().slice(0, 10));
     }, []);
 
-
+    const handleBuscarClick = () => {
+        obtenerServicios();
+    };
 
     return (
         <>
-
             <div className='container'>
-
-                <Link to={"agregar"}><button className='btn btn-success mt-3'>Agregar Servicio</button></Link>
+                <div className="row mt-3">
+                    <div className="col-md-2">
+                        <Link to={"agregar"}><button className='btn btn-success mt-3'>Agregar Servicio</button></Link>
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="fechaInicio">Fecha de inicio:</label>
+                        <input type="date" id="fechaInicio" value={fechaInicio} onChange={(e) => setFechaInicio(e.target.value)} className="form-control" />
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="fechaFin">Fecha de fin:</label>
+                        <input type="date" id="fechaFin" value={fechaFin} onChange={(e) => setFechaFin(e.target.value)} className="form-control" />
+                    </div>
+                    <div className="col-md-2">
+                        <button className='btn btn-primary mt-4' onClick={handleBuscarClick}>Buscar</button>
+                    </div>
+                </div>
+                <hr />
                 <div className='table-responsive'>
                     <table className="table">
                         <thead>
@@ -57,32 +79,35 @@ function ListControl() {
                                 <th scope="col">Pago</th>
                             </tr>
                         </thead>
-                        {datos !== null
-                            ? datos.map((datos) => (
-                                <tbody key={datos?._id}>
-                                    <tr>
-                                        <td>{datos.numeroControl}</td>
-                                        <td>{datos.fecha.substring(0, 10)}</td>
-                                        <td>{datos.cliente}</td>
-                                        <td>{datos.almacen}</td>
-                                        <td>{datos.maquina}</td>
-                                        <td>{datos.turno}</td>
-                                        <td>{datos.serviceType}</td>
-                                        <td>{datos.inicio}</td>
-                                        <td>{datos.fin}</td>
-                                        <td>{datos.total}</td>
-                                        <td>{datos.tarifa}</td>
-                                        <td>{datos.payO}</td>
+                        <tbody>
+                            {datos !== null ? (
+                                datos.map((servicio) => (
+                                    <tr key={servicio._id}>
+                                        <td>{servicio.numeroControl}</td>
+                                        <td>{servicio.fecha.substring(0, 10)}</td>
+                                        <td>{servicio.cliente}</td>
+                                        <td>{servicio.almacen}</td>
+                                        <td>{servicio.maquina}</td>
+                                        <td>{servicio.turno}</td>
+                                        <td>{servicio.serviceType}</td>
+                                        <td>{servicio.inicio}</td>
+                                        <td>{servicio.fin}</td>
+                                        <td>{servicio.total}</td>
+                                        <td>{servicio.tarifa}</td>
+                                        <td>{servicio.payO}</td>
                                     </tr>
-                                </tbody>
-                            ))
-                            : "no hay datos"}
-
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan='12'>No hay datos disponibles</td>
+                                </tr>
+                            )}
+                        </tbody>
                     </table>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default ListControl;
